c75a95f26704210b43629cc26f0e017f642d1472
Chandler Abril <abrilchandler1@gmail.com>
2023-10-31 20:01:37 -0700

    needs work

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
-function addingEventListener() {
+ function addingEventListener() {
     
     const input = document.getElementById('button');
 
 function clickAlert() {
-  alert('I was clicked!');
+ alert('I was clicked!');
 }
 
 input.addEventListener('click', clickAlert);
 
 }
+
